5477ae581d03e987eb06389f0fc6576bfc06da91
khalid <khalid.akhtar@hotmail.com>
2023-12-31 12:48:08 +0530

    Made changes to structure, Created package apppages to hold application pages

--- src/main/java/org/aka/gameshop/factory/BroFactory.java
+++ src/main/java/org/aka/gameshop/factory/BroFactory.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 
-public class PlaywrightFactory {
+public class BroFactory {
     Playwright playwright;
     Browser browser;
     BrowserContext browserContext;
