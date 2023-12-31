c5de9d2704f2bb92af0c63723e34e831edbe0df9
khalid <khalid.akhtar@hotmail.com>
2023-12-31 12:47:06 +0530

    Made changes to properties to support headless mode

--- src/test/java/org/aka/gameshop/base/BaseTest.java
+++ src/test/java/org/aka/gameshop/base/BaseTest.java
@@ -1,24 +1,22 @@
 package org.aka.gameshop.base;
 
 import com.microsoft.playwright.Page;
-import org.aka.gameshop.factory.PlaywrightFactory;
-import org.aka.gameshop.factory.pages.HomePage;
+import org.aka.gameshop.factory.BroFactory;
+import org.aka.gameshop.apppages.HomePage;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.util.Properties;
 
 public class BaseTest {
-    PlaywrightFactory playFactory;
+    BroFactory playFactory;
     Page page;
     protected Properties prop;
     protected HomePage homePage;
 
     @BeforeMethod
     public void setupBrowser(){
-        playFactory = new PlaywrightFactory();
+        playFactory = new BroFactory();
         prop = playFactory.rtEnvSetup();
         page = playFactory.initBrowser(prop);
         homePage = new HomePage(page);
