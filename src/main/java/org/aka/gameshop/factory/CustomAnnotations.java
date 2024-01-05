d6e2f001a0307ada29b060fc807637606f0ccf93
khalid <khalid.akhtar@hotmail.com>
2024-01-05 10:41:41 +0530

    Added custom annotation to pass test name to listeners

---
+++ src/main/java/org/aka/gameshop/factory/CustomAnnotations.java
@@ -0,0 +1,13 @@
+package org.aka.gameshop.factory;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+public class CustomAnnotations {
+
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.METHOD,ElementType.ANNOTATION_TYPE})
+    public @interface TestName { String value();
+    }
+}
--- src/test/java/org/aka/gameshop/tests/HomePageTest.java
+++ src/test/java/org/aka/gameshop/tests/HomePageTest.java
@@ -2,39 +2,45 @@ package org.aka.gameshop.tests;
 
 import org.aka.gameshop.appData.AppData;
 import org.aka.gameshop.base.BaseTest;
+import org.aka.gameshop.factory.CustomAnnotations.*;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.aka.gameshop.factory.Utils;
 
 public class HomePageTest extends BaseTest {
 
 
-    @Test(testName = "Check if the URL of ",priority = 1)
-    public void homeURLTest(){
-//        if (homePage==null)
-//            setupBrowser();
-        String homePageURL = homePage.getHomePageURL();
-        System.out.printf("Home page url is %s",homePageURL );
-        Assert.assertEquals(homePageURL,prop.getProperty("url"));
-    }
-    @Test(priority = 2)
-    public void homeTitleTest(){
+    @TestName("Verify that correct home page title is displayed")
+    @Test(priority = 1)
+    public void homeTitleTest() {
         String homePageTitle = homePage.getHomePageTitle();
-        System.out.printf("Home page title is %s",homePageTitle );
-        Assert.assertTrue(homePageTitle.contains(AppData.HOME_PAGE_TITLE));
+        System.out.printf("Home page title is %s", homePageTitle);
+        Utils.printLogs("Home title printed successfully...");
+        Assert.assertTrue(homePageTitle.contains(AppData.HOME_PAGE_TITLE), "Actual Title: " + homePageTitle
+                + "\nExpected to contain :" + AppData.HOME_PAGE_TITLE);
+
     }
 
-    @Test(dataProvider = "getSearchData",priority = 3)
-    public void homeSearchTest(String productName){
+    @TestName("Check if the URL of home page is correct")
+    @Test(priority = 2)
+    public void homeURLTest() {
+        String homePageURL = homePage.getHomePageURL();
+        System.out.printf("Home page url is %s", homePageURL);
+        Assert.assertEquals(homePageURL, prop.getProperty("url"));
+    }
+
+    @TestName("Verify that search functionality is working fine for")
+    @Test(dataProvider = "getSearchData", priority = 3)
+    public void homeSearchTest(String productName) {
         String searchResult = homePage.searchForProduct(productName);
-        System.out.printf("Searching for product %s",searchResult );
-        Assert.assertTrue(searchResult.contains(productName));
+        System.out.printf("Searching for product %s", searchResult);
+        Assert.assertTrue(searchResult.contains(productName), "Search Result: " + searchResult
+                + "\nExpected to contain :" + productName);
     }
 
     @DataProvider
-    public Object[][] getSearchData(){
-        return new Object[][] {{"PS5"},{"PS4"},{"Nintendo"}};
+    public Object[][] getSearchData() {
+        return new Object[][]{{"PS5"}, {"PS4"}, {"Nintendo"}};
     }
-
-
 }
