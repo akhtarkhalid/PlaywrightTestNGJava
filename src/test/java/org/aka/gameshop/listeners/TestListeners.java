0d4c7ea98b5e91219c79b79d96672199db8e18f4
khalid <khalid.akhtar@hotmail.com>
2024-01-05 10:48:27 +0530

    Added listener for updating extent report

---
+++ src/test/java/org/aka/gameshop/listeners/TestListeners.java
@@ -0,0 +1,64 @@
+package org.aka.gameshop.listeners;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.microsoft.playwright.Page;
+import org.aka.gameshop.factory.CustomAnnotations;
+import org.aka.gameshop.factory.Utils;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class TestListeners implements ITestListener {
+
+    ExtentReports extentReports = Utils.getReporter();
+    ExtentTest extentTest;
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        String testName ="";
+        Method method = result.getMethod().getConstructorOrMethod().getMethod();
+        if (method.isAnnotationPresent(CustomAnnotations.TestName.class)) {
+            CustomAnnotations.TestName testNameValue = method.getAnnotation(CustomAnnotations.TestName.class);
+            testName = testNameValue.value();
+        }else{
+            testName = "Test name missing for method :"+method.getName();
+        }
+        extentTest = extentReports.createTest(testName);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTest.log(Status.PASS,"Test Passed");
+        System.out.println("Result Attributes are");
+        Field[] myField= result.getTestClass().getRealClass().getFields();
+        System.out.println(Arrays.toString(myField));
+//        Page listenerPage;
+//        try {
+//           listenerPage = (Page)result.getTestClass().getRealClass().getField("Page").get(result.getInstance());
+//        } catch (IllegalAccessException | NoSuchFieldException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println(listenerPage.title());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.fail(result.getThrowable());
+    }
+
+    // Other methods of ITestListener
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Test execution finished");
+    }
+
+}
